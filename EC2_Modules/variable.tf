b774757d97c56ba5f801d1f976a0763c89700939
Abhi.kashyap <111049350+abhi-kashyap25@users.noreply.github.com>
2024-04-16 10:34:15 +0530

    Create variable.tf

---
+++ EC2_Modules/variable.tf
@@ -0,0 +1,23 @@
+variable "ami_value" {
+  description = "value for ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+  
+}
+
+variable "access_key" {
+    description = "value for access_key"
+  
+}
+
+variable "secret_key" {
+    description = "value for secret_key"
+  
+}
