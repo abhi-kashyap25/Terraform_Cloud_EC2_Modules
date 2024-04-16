da87dc4b5c3472af64a8a40ad3a3ca46ce79a796
Abhi.kashyap <111049350+abhi-kashyap25@users.noreply.github.com>
2024-04-16 10:33:40 +0530

    Update output.tf

--- EC2_Modules/output.tf
+++ EC2_Modules/output.tf
@@ -1,1 +1,12 @@
+output "public-ip-address" {
+    value = aws_instance.ec2.public_ip # Description: Retrieves the public IP address of the EC2 instance.
+}
+
+output "instance_type" {
+    value = aws_instance.ec2.instance_type # Description: Retrieves the instance type of the EC2 instance.
+}
+
+output "instance_az" {
+    value = aws_instance.ec2.availability_zone # Description: Retrieves the availability zone of the EC2 instance.
+}
 
